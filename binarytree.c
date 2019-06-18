d96d74e12336782874552a94669ca941766cedac
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-06-18 22:34:42 +0000

    added struct

---
+++ binarytree.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+//minichallenge create the NODE for a binary tree.
+typedef struct NODE
+{
+    int value;
+    struct NODE *low;
+    struct NODE *high;
+} NODE;
+
+NODE *rootNode;
+
+int main(void)
+{
+
+}
