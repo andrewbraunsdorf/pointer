57b395a33d105ce40d7363c79a90fec1e52bce8b
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-06-18 22:51:18 +0000

    added does contain function

--- binarytree.c
+++ binarytree.c
@@ -90,6 +90,46 @@ void displayTreeOnDeparture(NODE *curNode)
     printf("%i ", curNode->value);
 }
 
+int doesContain(int value)
+{
+    NODE *trav = rootNode;
+    while(1)
+    {
+        if (trav->value == value)
+        {
+            return 1;
+        }
+        if (value < trav->value)
+        {
+            if (trav->low == NULL)
+            {
+                return 0;
+            }
+            trav = trav->low;
+            continue;
+        }
+        if (!trav->high)
+        {
+            return 0;
+        }
+        trav = trav->high;
+        continue;
+    }
+}
+
+void tests()
+{
+    assert(doesContain(10) && "tree does contain 10");
+    assert(doesContain(8) && "tree does contain 8");
+    assert(doesContain(4) && "tree does contain 4");
+    assert(!doesContain(1) && "tree does not contain 1");
+    assert(doesContain(15) && "tree does contain 15");
+    assert(!doesContain(100) && "Tree does not contain 100");
+    assert(doesContain(13) && "tree does contain 13");
+    assert(doesContain(7) && "tree does contain 7");
+    assert(!doesContain(16) && "tree does not contain 16");
+}
+
 int main(void)
 {
     printf("hello\n");
@@ -111,4 +151,8 @@ int main(void)
 
     displayTreeOnDeparture(rootNode);
     printf("\n");
+
+    doesContain(10);
+
+    tests();
 }
