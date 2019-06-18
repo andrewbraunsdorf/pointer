6bb67f0a05750b360fb572fb3552597ab88015e9
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-06-18 22:36:35 +0000

    added add node function

--- binarytree.c
+++ binarytree.c
@@ -12,6 +12,52 @@ typedef struct NODE
 
 NODE *rootNode;
 
+void addNode(int value)
+{
+    NODE *newNode = malloc(sizeof(NODE));
+    newNode->value = value;
+    newNode->high = NULL;
+    newNode->low = NULL;
+
+    if (rootNode == NULL)
+    {
+        rootNode = newNode;
+        return;
+    }
+
+    // if value of new node is less the root node attach to low
+    // else add to high.
+    NODE *trav = rootNode;
+    // printf("trying to add node %i\n", newNode->value);
+    while(1)
+    {
+        if (newNode->value < trav->value)
+        {
+            if (trav->low == NULL)
+            {
+                // printf("found empty spot\n");
+                trav->low = newNode;
+                return;
+            }
+            // printf("found a low node valued: %i moving to it\n", trav->low->value);
+            trav = trav->low;
+            continue;
+        }
+        else
+        {
+            if (trav->high == NULL)
+            {
+                // printf("found empty spot\n");
+                trav->high = newNode;
+                return;
+            }
+            // printf("found a high node valued: %i moving to it\n", trav->high->value);
+            trav = trav->high;
+            continue;
+        }
+    }
+}
+
 int main(void)
 {
 
