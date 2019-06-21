f749c455562d36989ff153dd0e0d7ffabde24716
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-06-21 00:26:48 +0000

    add insert node

--- boolsearch.c
+++ boolsearch.c
@@ -11,3 +11,20 @@ bool	search(node*	list,	int	n)
     }
     return	false;
 }
+
+void insert(int n)
+{
+    // create new node
+    node *new = malloc(sizeof(node));
+    //check for NULL
+    if (new == NULL)
+    {
+        exit(1);
+    }
+    //	initialize	new	node
+    new->n = n;
+    new->next = NULL;
+    // insert new node at head
+    new->next = head;
+    head = new;
+}
